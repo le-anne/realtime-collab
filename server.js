2ba92b7b9aa14aca8c3aceee4372870000f637ec
Leanne <76232496+le-anne@users.noreply.github.com>
2023-05-04 12:01:40 -0700

    updated connection message

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer);
 const path = require("path");
 
-const HOST = '0.0.0.0'; // listen on all network interfaces
+const HOST = "0.0.0.0"; // listen on all network interfaces
 const PORT = process.env.PORT || 3000;
 
 httpServer.listen(PORT, HOST, () => {
   console.log(`Server listening on http://${HOST}:${PORT}`);
 });
-
 
 app.use(express.static("build"));
 app.get("/socket.io.js", (req, res) => {
@@ -38,5 +37,10 @@ io.on("connection", (socket) => {
     console.log("user disconnected", socket.id);
     io.emit("userStatus", "A user has left the text editor.");
     io.emit("userCount", userCount);
+  });
+
+  socket.on("text-change", (updatedText) => {
+    // emit the updated text to all other clients
+    socket.broadcast.emit("text-update", updatedText);
   });
 });
